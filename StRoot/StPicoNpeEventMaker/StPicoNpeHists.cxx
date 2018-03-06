9eb6f20c6fe7fcaa743249c25d94f691cbb6331d
Jongsik Eum <jongsikeum@me.com>
2018-03-06 19:47:39 +0900

    Add Event cut histograms to check whether cut is working or not.

--- StRoot/StPicoNpeEventMaker/StPicoNpeHists.cxx
+++ StRoot/StPicoNpeEventMaker/StPicoNpeHists.cxx
@@ -20,29 +20,49 @@ mh1TotalEventsInRun(NULL), mh1TotalHftTracksInRun(NULL), mh1TotalGRefMultInRun(N
 mh2InvariantMassVsPt(NULL)
 {
     mPrescales = new StPicoPrescales(cuts::prescalesFilesDirectoryName);
-    
+
     mOutFile = new TFile(Form("%s.picoNpe.hists.root",fileBaseName.Data()),"RECREATE");
-    
+
     int nRuns = mPrescales->numberOfRuns();
     TH1::SetDefaultSumw2();
+    // Run information.
     mh1TotalEventsInRun = new TH1F("mh1TotalEventsInRun","totalEventsInRun;runIndex;totalEventsInRun",nRuns+1,0,nRuns+1);
     mh1TotalHftTracksInRun = new TH1F("mh1TotalHftTracksInRun","totalHftTracksInRun;runIndex;totalHftTracksInRun",nRuns+1,0,nRuns+1);
     mh1TotalGRefMultInRun = new TH1F("mh1TotalGRefMultInRun","totalGRefMultInRun;runIndex;totalGRefMultInRun",nRuns+1,0,nRuns+1);
-    
+
     mh1TotalElectronsInRun = new TH1F("mh1TotalElectronsInRun","totalElectronsInRun;runIndex;totalElectronsInRun",nRuns+1,0,nRuns+1);
     mh1TotalPartnersInRun = new TH1F("mh1TotalPartnersInRun","totalPartnersInRun;runIndex;totalPartnersInRun",nRuns+1,0,nRuns+1);
     mh1TotalPhECandidatesInRun = new TH1F("mh1TotalPhECandidatesInRun","totalPhECandidatesInRun;runIndex;totalPhECandidatesInRun",nRuns+1,0,nRuns+1);
+
+    // Event cut QA plots
+    mh1QaEventCutTpcVz_raw = new TH1F("mh1QaEventCutTpcVz_raw", ";TPC_{V_{z}};", 100, -50, 50);
+    mh1QaEventCutTpcVz_cut = new TH1F("mh1QaEventCutTpcVz_cut", ";TPC_{V_{z}};", 100, -50, 50);
+    mh1QaEventCutTpcVz_fin = new TH1F("mh1QaEventCutTpcVz_fin", ";TPC_{V_{z}};", 100, -50, 50);
+
+    mh1QaEventCutVpdVz_raw = new TH1F("mh1QaEventCutVpdVz_raw", ";VPD_{V_{z}};", 100, -50, 50);
+    mh1QaEventCutVpdVz_cut = new TH1F("mh1QaEventCutVpdVz_cut", ";VPD_{V_{z}};", 100, -50, 50);
+    mh1QaEventCutVpdVz_fin = new TH1F("mh1QaEventCutVpdVz_fin", ";VPD_{V_{z}};", 100, -50, 50);
+
+    mh1QaEventCutDffVz_raw = new TH1F("mh1QaEventCutDffVz_raw", ";TPC_{V_{z}}-VPD_{V_{z}};", 100, -50, 50);
+    mh1QaEventCutDffVz_raw = new TH1F("mh1QaEventCutDffVz_raw", ";TPC_{V_{z}}-VPD_{V_{z}};", 100, -50, 50);
+    mh1QaEventCutDffVz_raw = new TH1F("mh1QaEventCutDffVz_raw", ";TPC_{V_{z}}-VPD_{V_{z}};", 100, -50, 50);
+
+    mh2QaEventCutTpcVzVsVpdVz_raw = TH2F("mh2QaEventCutTpcVzVsVpdVz_raw", ";TPC_{V_{z}};VPD_{V{_z}}", 100, -50, 50, 100, -50, 50);
+    mh2QaEventCutTpcVzVsVpdVz_cut = TH2F("mh2QaEventCutTpcVzVsVpdVz_cut", ";TPC_{V_{z}};VPD_{V{_z}}", 100, -50, 50, 100, -50, 50);
+    mh2QaEventCutTpcVzVsVpdVz_fin = TH2F("mh2QaEventCutTpcVzVsVpdVz_fin", ";TPC_{V_{z}};VPD_{V{_z}}", 100, -50, 50, 100, -50, 50);
+    // Event cut QA plots end
+
     mh2NElectronsVsNPartners = new TH2F("mh2NElectronsVsNPartners","nElectronsVsNPartners;nPartners;nElectrons",500,0,500,200,0,200);
-  
+
     mh2PairDcaVsPt = new TH2F("mh2PairDcaVsPt","PairDcaVsPt;p_{T}(Tagged e)(GeV/c);Pair DCA(cm))",120,0,12,100,0,1);
     mh2InvariantMassVsPt = new TH2F("mh2InvariantMassVsPt","InvariantMassVsPt;p_{T}(Tagged e)(GeV/c);Invarient Mass(GeV/c^2)",120,0,12,201,0,0.2);
     mh2ConversionPosition = new TH2F("mh2ConversionPosition","mh2ConversionPosition; x(cm); y(cm)",100,-10,10,100,-10,10);
-    
+
     mh2PairDcaVsPtQaCut = new TH2F("mh2PairDcaVsPtQaCut","PairDcaVsPtQaCut;p_{T}(Tagged e)(GeV/c);Pair DCA(cm))",120,0,12,100,0,1);
     mh2InvariantMassVsPtQaCut = new TH2F("mh2InvariantMassVsPtQaCut","InvariantMassVsPtQaCut;p_{T}(Tagged e)(GeV/c);Invarient Mass(GeV/c^2)",120,0,12,201,0,0.2);
     mh2ConversionPositionQaCut = new TH2F("mh2ConversionPositionQaCut","mh2ConversionPositionQaCut; x(cm); y(cm)",100,-10,10,100,-10,10);
-    
-    
+
+
 }
 StPicoNpeHists::~StPicoNpeHists()
 {
@@ -50,6 +70,34 @@ StPicoNpeHists::~StPicoNpeHists()
     // note that histograms are owned by mOutFile. They will be destructed
     // when the file is closed.
 }
+//-----------------------------------------------------------------------
+void StPicoNpeHists::fill1dHist(int type, float value, float value)
+{
+    switch(type){
+        case 0:
+            mh1QaEventCutTpcVz_cut->Fill(value);
+            break;
+        case 1:
+            mh1QaEventCutVpdVz_cut->Fill(value);
+            break;
+        case 2:
+            mh1QaEventCutDffVz_cut->Fill(value);
+            break;
+        case 3:
+            mh2QaEventCutTpcVzVsVpdVz_cut->Fill(value);
+            break;
+    }
+}
+//-----------------------------------------------------------------------
+void StPicoNpeHists::fill2dHist(int type, float xValue, float yValue)
+{
+    switch(type){
+        case 0:
+            mh2QaEventCutTpcVzVsVpdVz_cut->Fill(xValue, yValue);
+            break;
+    }
+}
+
 //-----------------------------------------------------------------------
 void StPicoNpeHists::addEvent(StPicoEvent const& picoEvent,StPicoNpeEvent const & picoNpeEvent,unsigned int const nHftTracks)
 {
@@ -61,7 +109,26 @@ void StPicoNpeHists::addEvent(StPicoEvent const& picoEvent,StPicoNpeEvent const 
     mh1TotalPartnersInRun->Fill(runIndex,picoNpeEvent.nPartners());
     mh1TotalPhECandidatesInRun->Fill(runIndex,picoNpeEvent.nElectronPair());
     mh2NElectronsVsNPartners->Fill(picoNpeEvent.nPartners(),picoNpeEvent.nElectrons());
-    
+}
+//---------------------------------------------------------------------
+void StPicoNpeHists::addEventCutQa(StPicoEvent const* picoEvent, int type)
+{
+  float tpcVz = picoEvent->primaryVertex().z();
+  float vpdVz = picoEvent->vzVpd();
+  float vzVpdVz = fabs(tpcVz-vpdVz);
+
+  if(type==0){                  // Raw plot
+    mh1QaEventCutTpcVz_raw->Fill(tpcVz);
+    mh1QaEventCutVpdVz_raw->Fill(vpdVz);
+    mh1QaEventCutDffVz_raw->Fill(vzVpdVz);
+    mh2QaEventCutTpcVzVsVpdVz_raw->Fill(tpcVz, vpdVz);
+  }
+  if(type==1){                  // Final plot
+    mh1QaEventCutTpcVz_fin->Fill(tpcVz);
+    mh1QaEventCutVpdVz_fin->Fill(vpdVz);
+    mh1QaEventCutDffVz_fin->Fill(vzVpdVz);
+    mh2QaEventCutTpcVzVsVpdVz_fin->Fill(tpcVz, vpdVz);
+  }
 }
 //---------------------------------------------------------------------
 void StPicoNpeHists::addElectronPair(StElectronPair const* const epair, float electronPt, bool const fillMass)
@@ -85,7 +152,7 @@ void StPicoNpeHists::closeFile()
     mh1TotalPartnersInRun->Write();
     mh1TotalPhECandidatesInRun->Write();
     mh2NElectronsVsNPartners->Write();
-    
+
     mh2PairDcaVsPt->Write();
     mh2InvariantMassVsPt->Write();
     mh2ConversionPosition->Write();
@@ -93,6 +160,6 @@ void StPicoNpeHists::closeFile()
     mh2PairDcaVsPtQaCut->Write();
     mh2InvariantMassVsPtQaCut->Write();
     mh2ConversionPositionQaCut->Write();
-    
+
     mOutFile->Close();
 }
--- StRoot/StPicoNpeEventMaker/StPicoNpeHists.h
+++ StRoot/StPicoNpeEventMaker/StPicoNpeHists.h
@@ -27,35 +27,54 @@ class StElectronPair;
 
 class StPicoNpeHists: public TObject
 {
-public:
-    StPicoNpeHists(TString fileBaseName);
-    virtual ~StPicoNpeHists();
-    void addEvent(StPicoEvent const &, StPicoNpeEvent const &,unsigned int const nHftTracks);
-    void addElectronPair(StElectronPair const*, float electronPt, bool fillMass);
-    void closeFile();
+ public:
+  StPicoNpeHists(TString fileBaseName);
+  virtual ~StPicoNpeHists();
+  void addEvent(StPicoEvent const &, StPicoNpeEvent const &,unsigned int const nHftTracks);
+  void addEventQa(StPicoEvent const*, int type);
+  void addElectronPair(StElectronPair const*, float electronPt, bool fillMass);
+  void closeFile();
+
+ private:
+  StPicoNpeHists(){}
+
+  StPicoPrescales* mPrescales;
+  TFile* mOutFile;
+  TH1F* mh1TotalEventsInRun;
+  TH1F* mh1TotalHftTracksInRun;
+  TH1F* mh1TotalGRefMultInRun;
+
+  TH1F* mh1TotalElectronsInRun;
+  TH1F* mh1TotalPartnersInRun;
+  TH1F* mh1TotalPhECandidatesInRun;
+
+  // Event cut QA plots
+  TH1F* mh1QaEventCutTpcVz_raw;
+  TH1F* mh1QaEventCutTpcVz_cut;
+  TH1F* mh1QaEventCutTpcVz_fin;
+
+  TH1F* mh1QaEventCutVpdVz_raw;
+  TH1F* mh1QaEventCutVpdVz_cut;
+  TH1F* mh1QaEventCutVpdVz_fin;
+
+  TH1F* mh1QaEventCutDffVz_raw;
+  TH1F* mh1QaEventCutDffVz_cut;
+  TH1F* mh1QaEventCutDffVz_fin;
+
+  TH2F* mh2QaEventCutTpcVzVsVpdVz_raw;
+  TH2F* mh2QaEventCutTpcVzVsVpdVz_cut;
+  TH2F* mh2QaEventCutTpcVzVsVpdVz_fin;
+  // Event cut QA plots end
+
+  TH2F* mh2NElectronsVsNPartners;
+  TH2F* mh2PairDcaVsPt;
+  TH2F* mh2InvariantMassVsPt;
+  TH2F* mh2ConversionPosition;
     
-private:
-    StPicoNpeHists(){}
+  TH2F* mh2PairDcaVsPtQaCut;
+  TH2F* mh2InvariantMassVsPtQaCut;
+  TH2F* mh2ConversionPositionQaCut;
     
-    StPicoPrescales* mPrescales;
-    TFile* mOutFile;
-    TH1F* mh1TotalEventsInRun;
-    TH1F* mh1TotalHftTracksInRun;
-    TH1F* mh1TotalGRefMultInRun;
-    
-    TH1F* mh1TotalElectronsInRun;
-    TH1F* mh1TotalPartnersInRun;
-    TH1F* mh1TotalPhECandidatesInRun;
-    
-    TH2F* mh2NElectronsVsNPartners;
-    TH2F* mh2PairDcaVsPt;
-    TH2F* mh2InvariantMassVsPt;
-    TH2F* mh2ConversionPosition;
-    
-    TH2F* mh2PairDcaVsPtQaCut;
-    TH2F* mh2InvariantMassVsPtQaCut;
-    TH2F* mh2ConversionPositionQaCut;
-    
-    ClassDef(StPicoNpeHists, 2)
-};
+  ClassDef(StPicoNpeHists, 2)
+    };
 #endif
